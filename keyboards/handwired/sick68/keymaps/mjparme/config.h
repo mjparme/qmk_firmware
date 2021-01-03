958e35fa7820f7cc007cad11e5f9995dbb64c55f
Michael Parmeley <michael.parmeley@ncr.com>
2021-01-03 17:49:54 -0600

    first version

---
+++ keyboards/handwired/sick68/keymaps/mjparme/config.h
@@ -0,0 +1,26 @@
+/*
+Copyright 2020 mjparme
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+#define MANUFACTURER mjparme
+
+//B7 is not available on a ProcMicro but is one of the extra pins on an Elite-C
+ #define BACKLIGHT_PIN B7
+ #define BACKLIGHT_LEVELS 3
---
+++ keyboards/handwired/sick68/keymaps/mjparme/rules.mk
@@ -0,0 +1,15 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+BOOTLOADER = qmk-dfu
+
+BACKLIGHT_ENABLE = yes
+LAYOUTS = 65_ansi
