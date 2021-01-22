ad9cc0be464607cfc865a7714eb7e34df2b38e85
Michael Parmeley <michael.parmeley@ncr.com>
2021-01-21 22:38:27 -0600

    added a reset key and make h, j, k, l arrows on the _FN layer

--- keyboards/handwired/sick68/keymaps/mjparme/config.h
+++ keyboards/handwired/sick68/keymaps/mjparme/config.h
@@ -19,11 +19,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-#define MANUFACTURER mjparme
+#undef MATRIX_COL_PINS
+#undef UNUSED_PINS
 
 //Uses the Elite-C instead of ProMicro so has a few unused pins
-#define UNUSED_PIN {D5, C7, F1, F0, B0}
+#define MATRIX_COL_PINS \
+    { C6, D7, E6, B4, B5, D5, C7, B6, B2, B3, B1, F7, F6, F5, F4 }
+#define UNUSED_PIN {C7, F1, F0}
 
 //B7 is not available on a ProcMicro but is one of the extra pins on an Elite-C
  #define BACKLIGHT_PIN B7
- #define BACKLIGHT_LEVELS 3
+ #define BACKLIGHT_LEVELS 5
